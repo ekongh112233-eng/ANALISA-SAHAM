01fe72d9bf9587ccabeffab0adc5e878c5709b0b
ekongh112233-eng <ekongh112233@gmail.com>
2026-07-30 12:23:14 +0000

    fdhdh

--- jalankan_bot.sh
+++ jalankan_bot.sh
@@ -6,6 +6,12 @@ cd /home/kaltaraid/Documents/ANALISA-SAHAM/
 # Jalankan skrip Python
 /usr/bin/python update_data.py
 
+# ==========================================
+# FITUR SAPU OTOMATIS (MAX 50 HARI)
+# Menghari file .csv di folder arsip yang umurnya lebih dari 50 hari
+find Arsip_Data_Harian/ -name "*.csv" -type f -mtime +50 -delete
+# ==========================================
+
 # Simpan dan kirim ke GitHub
 git pull origin main
 git add hasil_screener.csv
